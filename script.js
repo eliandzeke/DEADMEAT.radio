5ef3947c50c10d65be2efbe210eadde5ce5aa976
eliandzeke <96255650+eliandzeke@users.noreply.github.com>
2025-10-09 19:44:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const button = document.getElementById("button");
+const clicks = document.getElementById("clicks");
+
+let clickCount = 0
+
+button.addEventListener("click", function(){
+    clickCount++;
+    clicks.innerHTML = clickCount;
+});
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
